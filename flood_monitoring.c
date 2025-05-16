d9feb9a120ea93860579343c08cec21f774d0b24
Caique <caiquedebritoo@gmail.com>
2025-05-16 16:02:48 -0300

    feat: add display task for joystick data visualization on SSD1306

--- flood_monitoring.c
+++ flood_monitoring.c
@@ -4,6 +4,8 @@
 #include "task.h"
 #include "queue.h"
 #include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
 
 // Modo BOOTSEL com botão B
 #include "pico/bootrom.h"
@@ -15,6 +17,11 @@
 #define ADC_JOYSTICK_Y_CHANNEL 0
 #define ADC_JOYSTICK_X_CHANNEL 1
 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define I2C_ADDRESS 0x3C
+
 typedef struct
 {
     uint16_t water_level; // Simula o nível da água no eixo X
@@ -25,6 +32,7 @@ QueueHandle_t xQueueJoystickData;
 
 void gpio_irq_handler(uint gpio, uint32_t events);
 void vJoystickTask(void *params);
+void vDisplayTask(void *params);
 
 int main()
 {
@@ -42,8 +50,9 @@ int main()
 
     xQueueJoystickData = xQueueCreate(10, sizeof(joystick_data_t)); // Cria a fila para os dados do joystick
 
-   // Criação das tasks
+    // Criação das tasks
     xTaskCreate(vJoystickTask, "Joystick Task", 256, NULL, 1, NULL);
+    xTaskCreate(vDisplayTask, "Display Task", 256, NULL, 1, NULL);
 
     // Inicia o agendador
     vTaskStartScheduler();
@@ -72,5 +81,36 @@ void vJoystickTask(void *params)
 
         xQueueSend(xQueueJoystickData, &joystick_data, portMAX_DELAY); // Envia o valor do joystick para a fila
         vTaskDelay(pdMS_TO_TICKS(200));                        // 10 Hz de leitura
+    }
+}
+
+void vDisplayTask(void *params)
+{
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, I2C_ADDRESS, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_send_data(&ssd);
+
+    joystick_data_t joystick_data;
+
+    while (true)
+    {
+        if (xQueueReceive(xQueueJoystickData, &joystick_data, portMAX_DELAY) == pdTRUE)
+        {
+            char water_level_str[12];
+            char rain_volume_str[12];
+            snprintf(water_level_str, sizeof(water_level_str), "Water: %d", joystick_data.water_level);
+            snprintf(rain_volume_str, sizeof(rain_volume_str), "Rain: %d", joystick_data.rain_volume);
+            ssd1306_draw_string(&ssd, water_level_str, 2, 28);
+            ssd1306_draw_string(&ssd, rain_volume_str, 2, 36);
+            ssd1306_send_data(&ssd);
+        }
+        vTaskDelay(pdMS_TO_TICKS(100)); // Atualiza a tela a cada 100ms
     }
 }
